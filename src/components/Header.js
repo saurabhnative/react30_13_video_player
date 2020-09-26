1b68f7a30e39434abf6a80480321fa8453f3fb76
codeclassifiers <codeclassifiers@gmail.com>
2020-09-26 18:06:15 +0530

    blueprint added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="w-100 text-light">SpaceX launch</div>
+        </nav>
+    );
+}
+  
+export default Header;
+  
---
+++ src/components/LaunchInfo.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+function LaunchInfo() {
+    return(<div></div>)
+}
+
+export default LaunchInfo;
